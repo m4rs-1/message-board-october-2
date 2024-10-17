ed1980b21399e5966028e8126379a8b45076ca99
m4rs-1 <mrujano04@gmail.com>
2024-10-17 18:17:02 -0400

    post detail & new post link

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostCreateView
+from .views import PostListView, PostCreateView, PostDetailView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
     path("post/new/", PostCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
 ]
